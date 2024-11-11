2408902e9d44fe35f529edfbd2e2bfb68460856e
kolyunyauser015 <89058507+kolyunyauser015@users.noreply.github.com>
2024-11-11 09:28:40 +0600

    Update keyboards.py

--- module_14_3/keyboards.py
+++ module_14_3/keyboards.py
@@ -3,25 +3,27 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 start_kb = ReplyKeyboardMarkup(
-    keyboard=[[KeyboardButton(text='Рассчитать'),
-               KeyboardButton(text='Информация')]],
+    keyboard=[
+        [KeyboardButton(text='Рассчитать'),
+         KeyboardButton(text='Информация')],
+        [InlineKeyboardButton(text='Купить')]
+    ],
     resize_keyboard=True)
-start_kb.add(KeyboardButton(text='Купить'))
 
 
-calories_kb = InlineKeyboardMarkup(resize_keyboard=True)
-button_1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
-button_2 = InlineKeyboardButton(text='Формулы расчета', callback_data='formulas')
-calories_kb.add(button_1)
-calories_kb.insert(button_2)
+calories_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [KeyboardButton(text='Рассчитать норму калорий', callback_data='calories'),
+        KeyboardButton(text='Формулы расчета', callback_data='formulas')]
+    ],
+    resize_keyboard=True)
 
 
-catalog_kb = InlineKeyboardMarkup(resize_keyboard=True, row_width=4)
-button_product_1 = InlineKeyboardButton(text='Product1', callback_data="product_buying")
-button_product_2 = InlineKeyboardButton(text='Product2', callback_data="product_buying")
-button_product_3 = InlineKeyboardButton(text='Product3', callback_data="product_buying")
-button_product_4 = InlineKeyboardButton(text='Product4', callback_data="product_buying")
-catalog_kb.add(button_product_1)
-catalog_kb.insert(button_product_2)
-catalog_kb.insert(button_product_3)
-catalog_kb.insert(button_product_4)
+catalog_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Product1', callback_data="product_buying"),
+        InlineKeyboardButton(text='Product2', callback_data="product_buying"),
+        InlineKeyboardButton(text='Product3', callback_data="product_buying"),
+        InlineKeyboardButton(text='Product4', callback_data="product_buying")]
+    ],
+    resize_keyboard=True)
